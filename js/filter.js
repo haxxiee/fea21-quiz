c9524dfa59d5c975c564b3030f58b94a70268720
Linus Foxell <linusfoxell@hotmail.com>
2022-12-22 16:04:44 +0100

    vanilla JS klar

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,3 @@
-
-
 // Skriv en funktion som tar bort räliga matvaror från
 // en given array och returnerar en ny array med acceptabel mat.
 // Använd dig av .filter()
@@ -8,10 +6,16 @@ const nastyFoods = ["Pannkakor", "Lingonsylt", "Fläsksvålar"];
 
 function noNastyFood(array) {
   // Skriv din kod här
-
+  return array.filter((item) => !nastyFoods.includes(item));
 }
 
-const result = noNastyFood(["Korv", "Sylta", "Pannkakor", "Chips", "Lingonsylt"]);
+const result = noNastyFood([
+  "Korv",
+  "Sylta",
+  "Pannkakor",
+  "Chips",
+  "Lingonsylt",
+]);
 console.log(result); // ["Korv", "Sylta", "Chips"]
 
 module.exports = noNastyFood;
--- js/object.js
+++ js/object.js
@@ -1,19 +1,20 @@
-
 // Rör ej
 const person = {
   firstName: "Oscar",
   lastName: "Nilsson",
-  age: 30
-}
+  age: 30,
+};
 
 // Skriv klart funktionen som skriver ut mitt fulla namn
 // OBS: Du får inte ändra funktions-parametrarna.
 function printName(first, last) {
   // Skriv din kod här
-
+  return `${first} ${last}`;
 }
 
 // Du kommer även behöva ändra anropet här nedan
-printName(person.firstName, person.lastName) // "Oscar Nilsson"
+// Inte helt säger om detta är det ni vill
+console.log(printName(person.firstName, person.lastName)); // "Oscar Nilsson"
+printName(person.firstName, person.lastName); // "Oscar Nilsson"
 
-module.exports = printName
+module.exports = printName;
